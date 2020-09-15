3434aa93a2a36eed325ee88db3556103a62f8af1
Ogabrielmonteiro <gabriel.bsilva@bandtec.com.br>
2020-09-15 14:57:10 -0300

    retirado a tabela login do BD

--- BD-BANCADA-REFRIGERADA.sql
+++ BD-BANCADA-REFRIGERADA.sql
@@ -22,29 +22,6 @@ select * from cadastro;
 -- Excluir tabela cadastro
 drop table cadastro;
 
-
-
--- Criar tabela login --
-create table login (
-	cpf int primary key,
-    email varchar(80) not null,
-    senha varchar(20) not null,
-    pk_cpf int not null,
-    foreign key (pk_cpf) references cadastro (pk_cpf)
-);
-
--- Exibir tabela login
-select * from login;
-
--- Excluir tabela login
-drop table login;
-
--- Inserir dados da tabela login
-insert into login values
-("5164","gabriel monteiro","gabriel@gmail.com","euamocozinha","2001-06-09");
-
-
-
 -- Criar tabela frutas --
 create table fruta (
 	id_fruta int primary key auto_increment,
@@ -55,15 +32,16 @@ create table fruta (
 -- Inserir dados na tabela fruta
 insert into fruta values
 
-(null,"pera",1),
+(null,"pera",1), 
 (null,"maçâ",1),
 (null,"pêssego",1),
 (null,"laranja",2),
 (null,"mexirica",2),
 (null,"vagem",2),
-(null,"banana",3),
+(null,"banana",3), 
 (null,"mamão",3),
 (null,"limão",3);
+
 
 -- Excluir tabela	
 drop table fruta;
@@ -87,19 +65,17 @@ create table temp_grafico (
 	DT date not null  
 );
 
+-- Mostrar dados da tabela temp_grafico
 select * from temp_grafico;
 
-drop table temp_grafico;
-
+-- Inserir dados na tabela
 insert into temp_grafico value 
 (null,"5,30","10:54:00","2020-09-15"),
 (null,"5,32","10:54:01","2020-09-15"),
 (null,"5,31","10:54:02","2020-09-15"),
 (null,"5,35","10:54:03","2020-09-15"),
 (null,"5,28","10:54:04","2020-09-15"),
-(null,"5,29","10:54:05","2020-09-15");
-
-insert into temp_grafico value 
+(null,"5,29","10:54:05","2020-09-15"),
 (null,"5,20","10:54:00","2020-09-16"),
 (null,"5,25","10:54:01","2020-09-16"),
 (null,"5,30","10:54:02","2020-09-16"),
@@ -107,7 +83,9 @@ insert into temp_grafico value
 (null,"5,31","10:54:04","2020-09-16"),
 (null,"5,27","10:54:05","2020-09-16");
 
+-- Mostrar apenas a temperatura do grafico 
 select temperatura from temp_grafico where DT = "2020-09-16";
 
-
+-- Deletar tabela
+drop table temp_grafico;
 
